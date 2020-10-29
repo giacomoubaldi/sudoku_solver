a45ca86135382136c194e840b955470a7256a82a
Giacomo Ubaldi <giacomoubaldi@gmail.com>
2020-10-29 11:16:24 +0100

    add check function

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,1 +1,12 @@
-ECHO attivo.
+
+
+
+def check(array):
+    val = [1,2,3,4,5,6,7,8,9]
+    array.sort()
+    if val == array:
+        res = True
+    else:
+        res = False
+      
+    return res
