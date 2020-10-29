177dc8a3d7c4c2a013b4ffa235869127f430a2b8
Giacomo Ubaldi <giacomoubaldi@gmail.com>
2020-10-29 12:29:36 +0100

    change directory

---
+++ test_sudoku.py
@@ -0,0 +1,10 @@
+import pytest
+from .prova.sudoku.sudoku import check
+
+
+
+
+def test_check():
+    assert check([1,2,3,4,5,6,7,8,9]) == True
+    assert check([1,2,4,4,5,3,7,8,9]) == False
+    assert check([1,2,0,4,5,3,0,8,9]) == False
