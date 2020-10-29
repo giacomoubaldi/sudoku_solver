522736b886b08a4eff4ea93d2b2dadd7474a0058
Giacomo Ubaldi <giacomoubaldi@gmail.com>
2020-10-29 16:06:50 +0100

    add check columns and raws

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,4 @@
 
-
-
 def check(array):
     val = [1,2,3,4,5,6,7,8,9]
     array.sort()
@@ -10,3 +8,57 @@ def check(array):
         res = False
       
     return res
+
+
+def check_raws(table):
+    for i in range(9):
+        if(check(table[i]) == False):
+            return False
+    
+    return True
+        
+    
+def check_columns(table):
+    column = []
+    for i in range(9):
+        for j in range(9):
+            column.append(table[i][j])
+        
+        if(check(table[i]) == False):
+            return False
+    
+    return True
+
+
+
+array= [
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]
+    
+
+
+array[0]
+
+
+"""
+    validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]); // => true
+    """
+    
